64b8ee51f9a734336d267c053ad361f40ffb52af
mmozos <martinmozos@gmail.com>
2023-11-29 13:28:07 +0100

    Used ESlint and added Prettier plugin that sort CSS properties

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
-module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+// @see https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = withBundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
   },
-  output: "standalone",
-};
+  poweredByHeader: false,
+  trailingSlash: true,
+  assetPrefix,
+  basePath,
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  output: 'standalone',
+});
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import Link from "next/link";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <Link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
-            rel="stylesheet"
-          />
+          <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
